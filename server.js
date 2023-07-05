28461fcf747d35f41d0e68776a1ffc8db7400023
KipronohVincent <kipronohvincent43@gmail.com>
2023-07-05 12:46:48 +0300

    implementation of the CRUD operations

--- server.js
+++ server.js
@@ -1,115 +1,99 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
-
-app.use(bodyParser.json());
-
-// Import PostgreSQL library
 const { Pool } = require('pg');
+
+// Create a pool to manage database connections
 const pool = new Pool({
   user: 'vincent',
   host: 'localhost',
   database: 'onlinebookstore',
   password: 'new_password',
-  port: 5432,
+  port: 5432 
 });
 
 // Retrieve all books along with their corresponding author and category information
-app.get('/books', (req, res) => {
-  const query = `
-    SELECT b.title, a.name AS author, c.name AS category
-    FROM books b
-    JOIN authors a ON b.author_id = a.id
-    JOIN categories c ON b.category_id = c.id;
-  `;
-
-  pool.query(query, (error, results) => {
-    if (error) {
-      console.error('Error executing query', error);
-      res.status(500).send('Internal Server Error');
-    } else {
-      res.json(results.rows);
-    }
-  });
+app.get('/books', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query(`
+      SELECT books.title, authors.name AS author, categories.name AS category
+      FROM books
+      JOIN authors ON books.author_id = authors.id
+      JOIN categories ON books.category_id = categories.id
+    `);
+    client.release();
+    res.json(result.rows);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred' });
+  }
 });
 
 // Retrieve all authors who have written books in a specific category
-app.get('/authors/:category', (req, res) => {
+app.get('/authors/:category', async (req, res) => {
   const category = req.params.category;
-  const query = `
-    SELECT DISTINCT a.name
-    FROM books b
-    JOIN authors a ON b.author_id = a.id
-    JOIN categories c ON b.category_id = c.id
-    WHERE c.name = 'Fiction';
-  `;
-
-  pool.query(query, [category], (error, results) => {
-    if (error) {
-      console.error('Error executing query', error);
-      res.status(500).send('Internal Server Error');
-    } else {
-      res.json(results.rows);
-    }
-  });
+  try {
+    const client = await pool.connect();
+    const result = await client.query(`
+      SELECT authors.name
+      FROM authors
+      JOIN books ON authors.id = books.author_id
+      JOIN categories ON books.category_id = categories.id
+      WHERE categories.name = $1
+    `, [category]);
+    client.release();
+    res.json(result.rows);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred' });
+  }
 });
 
-// Insert a new book into the database with the necessary author and category references
-app.post('/books', (req, res) => {
-  const { title, author_id, category_id, price, publication_date } = req.body;
-  const query = `
-    INSERT INTO books (title, author_id, category_id, price, publication_date)
-    VALUES ('New Book', 1, 2, 24.99, '2022-04-01')`;
-
-  pool.query(query, [title, author_id, category_id, price, publication_date], (error, results) => {
-    if (error) {
-      console.error('Error executing query', error);
-      res.status(500).send('Internal Server Error');
-    } else {
-      res.json({ id: results.rows[0].id });
-    }
-  });
+// Insert a new book into the database
+app.post('/books', async (req, res) => {
+  const { title, authorId, categoryId, price, publicationDate } = req.body;
+  try {
+    const client = await pool.connect();
+    await client.query(
+      'INSERT INTO books (title, author_id, category_id, price, publication_date) VALUES ($1, $2, $3, $4, $5)',
+      [title, authorId, categoryId, price, publicationDate]
+    );
+    client.release();
+    res.sendStatus(201);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred' });
+  }
 });
 
 // Update the price of a specific book
-app.put('/books/:id', (req, res) => {
+app.put('/books/:id', async (req, res) => {
   const id = req.params.id;
   const { price } = req.body;
-  const query = `
-    UPDATE books
-    SET price = 29.99
-    WHERE id = 1;
-  `;
-
-  pool.query(query, [price, id], (error) => {
-    if (error) {
-      console.error('Error executing query', error);
-      res.status(500).send('Internal Server Error');
-    } else {
-      res.sendStatus(200);
-    }
-  });
+  try {
+    const client = await pool.connect();
+    await client.query(
+      'UPDATE books SET price = $1 WHERE id = $2',
+      [price, id]
+    );
+    client.release();
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred' });
+  }
 });
 
 // Delete a book from the database
-app.delete('/books/:id', (req, res) => {
+app.delete('/books/:id', async (req, res) => {
   const id = req.params.id;
-  const query = `
-    DELETE FROM books
-    WHERE id = 3;
-  `;
-
-  pool.query(query, [id], (error) => {
-    if (error) {
-      console.error('Error executing query', error);
-      res.status(500).send('Internal Server Error');
-    } else {
-      res.sendStatus(200);
-    }
-  });
+  try {
+    const client = await pool.connect();
+    await client.query('DELETE FROM books WHERE id = $1', [id]);
+    client.release();
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred' });
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
